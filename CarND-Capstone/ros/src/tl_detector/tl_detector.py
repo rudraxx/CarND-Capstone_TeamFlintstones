200e52107727d274ea39675af75a23a59a5c8e2c
jjaviergalvez <jjaviergalvez@hotmail.com>
2017-10-26 12:24:42 -0600

    feat: traffic light detection node without a classifier.

--- CarND-Capstone/ros/src/tl_detector/tl_detector.py
+++ CarND-Capstone/ros/src/tl_detector/tl_detector.py
@@ -10,6 +10,7 @@ from light_classification.tl_classifier import TLClassifier
 import tf
 import cv2
 import yaml
+import math
 
 STATE_COUNT_THRESHOLD = 3
 
@@ -90,18 +91,70 @@ class TLDetector(object):
             self.upcoming_red_light_pub.publish(Int32(self.last_wp))
         self.state_count += 1
 
-    def get_closest_waypoint(self, pose):
+    def get_closest_waypoint(self, stop_line_position):
         """Identifies the closest path waypoint to the given position
             https://en.wikipedia.org/wiki/Closest_pair_of_points_problem
         Args:
-            pose (Pose): position to match a waypoint to
+            stop_line_position : position to match a waypoint to
 
         Returns:
             int: index of the closest waypoint in self.waypoints
 
         """
-        #TODO implement
-        return 0
+
+        closest_wp_dist = 10000.0;
+        closest_wp_idx  = -1;
+        for idx,waypt in enumerate(self.waypoints.waypoints):
+            delta_x = stop_line_position[0] - waypt.pose.pose.position.x
+            delta_y = stop_line_position[1] - waypt.pose.pose.position.y
+            dist_to_wp = math.sqrt(delta_x*delta_x + delta_y*delta_y)
+
+            if (dist_to_wp < closest_wp_dist):
+                closest_wp_dist = dist_to_wp
+                closest_wp_idx  = idx
+
+        # Javi_note: perhaps the inex before closest_wp_idx need to be taken 'cause
+        # this position of the car probably is not the front of the car
+
+        return closest_wp_idx
+
+
+    def get_next_stop_line(self, pose, stop_line_positions):
+        # receive the car position and the stop lane positions array
+        # and return the closest dist and the index ahead of the vehicle
+
+        closest_dist = 10000.0;
+        closest_idx  = -1;
+        for i in range(len(stop_line_positions)):
+            dx = pose.position.x - stop_line_positions[i][0]
+            dy = pose.position.y - stop_line_positions[i][1]
+            dist = math.sqrt(dx*dx + dy*dy)
+
+            if (dist < closest_dist):
+                closest_dist = dist
+                closest_idx  = i
+
+        heading = math.atan2((stop_line_positions[closest_idx][1] - pose.position.y),
+                             (stop_line_positions[closest_idx][0] - pose.position.x))
+
+        quaternion = (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
+        _, _, yaw = tf.transformations.euler_from_quaternion(quaternion)
+
+        angle = abs(yaw - heading)
+        if angle > (math.pi / 4):
+            closest_idx += 1
+            dx = pose.position.x - stop_line_positions[closest_idx][0]
+            dy = pose.position.y - stop_line_positions[closest_idx][1]
+            closest_dist = math.sqrt(dx*dx + dy*dy)
+
+        return closest_dist, closest_idx
+
+    def get_light_patch(self):
+        # TODO team: implemet a function that take a patch of the self.camera_image. Here is where
+        # could be useful self.lights to take a more precise patch.
+
+        return None
+
 
     def get_light_state(self, light):
         """Determines the current color of the traffic light
@@ -135,10 +188,25 @@ class TLDetector(object):
 
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
-        if(self.pose):
-            car_position = self.get_closest_waypoint(self.pose.pose)
+        if(self.pose and self.waypoints):
+            #TODO find the closest visible traffic light (if one exists)
 
-        #TODO find the closest visible traffic light (if one exists)
+            # find the next probable stop
+            next_stop_line_dist, next_stop_line_idx = self.get_next_stop_line(self.pose.pose, stop_line_positions)
+
+            #rospy.loginfo("Javi: next probable stop dist = %s" %next_stop_line_dist)
+
+            # if the next probable stop is near of 50 mts (tweak this param if needed)
+            if next_stop_line_dist < 50:
+                # Get the waypoint index that we are going to send to the next node
+                light_wp = self.get_closest_waypoint(stop_line_positions[next_stop_line_idx])
+
+                light = self.get_light_patch() # TODO
+
+                # This two lines of code are only for DEBUGGING purpose as it will ignore the classfier.
+                # Comment it in the final version.
+                state = self.lights[next_stop_line_idx].state
+                return light_wp, state
 
         if light:
             state = self.get_light_state(light)
